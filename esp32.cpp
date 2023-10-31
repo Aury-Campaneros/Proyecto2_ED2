828f2b490cd2a6569bba960401aa8bc4e67167f8
Aury-Campaneros <133686899+Aury-Campaneros@users.noreply.github.com>
2023-10-31 00:43:17 -0600

    Add files via upload

--- esp32.cpp
+++ esp32.cpp
@@ -20,7 +20,7 @@
 // Variables Globales
 //****************************************************************
 volatile int sensor=0;  //Lectura del sensor
-float temp=0;  //guarda la conversión del sensor de temperatura
+float temp=0.00;  //guarda la conversión del sensor de temperatura
 int solicitar=0;
 
 //****************************************************************
@@ -35,7 +35,7 @@ int solicitar=0;
 void setup()
 {
   Serial.begin(115200);
-  Serial2.begin(9600); //PASAR DE 9600 A 115200 para comunicación UART2
+  Serial2.begin(115200); //PASAR DE 9600 A 115200 para comunicación UART2
   //Declaración de Entradas
   pinMode(34, INPUT); //Entrada del sensor
 }
@@ -46,7 +46,7 @@ void loop()
 {
   //Parte 1 - Comunicación con el Sensor y el ESP32
    sensor=analogRead(34);   //lectura del sensor
-   temp = ((sensor*3.3/4096)*(1/0.1))+20;   //conversicón de temp.
+   temp = ((sensor*3.3/4096)*(1/0.1))+22;   //conversicón de temp.
    //Parte 2 - Comunicación UART Compu y Tiva
    //Recibir datos
    if(Serial2.available()>0){
@@ -54,16 +54,9 @@ void loop()
   }
   //Transmitir datos
   if(solicitar==1){
-    Serial.println(temp);
     Serial2.print(temp);
-    Serial2.print('\n');
+    Serial.println(temp);
   }
-  if(solicitar==0){
-    Serial.println(0);
-    Serial2.print("");
-    Serial2.print('\n');
-  }
-  
 }
 //****************************************************************
 // Funciones
